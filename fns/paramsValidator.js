e35fcb69bc7ac6522ea50f2c713595cfc68430c0
Jithin Zacharia <jithin.mscis3@iiitmk.ac.in>
2019-08-08 15:29:22 +0530

    Adding params validator

---
+++ fns/paramsValidator.js
@@ -0,0 +1,16 @@
+/**
+ * @exports paramsValidator
+ * @status expiremental
+ * @desc Validator function for checking if the
+ * params passed are required or not.
+ * @param {Object | String} incoming 
+ * @param {Object | String} required 
+ * @returns {Boolean}
+ */
+exports.paramsValidator = (incoming, required)=>{
+    if(JSON.stringify(incoming) === JSON.stringify(Object.keys(required))){
+        return true;
+    }else{
+        return false;
+    }
+}
